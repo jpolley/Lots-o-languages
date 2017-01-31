0193f5de4eff97c5409d2620c51e605a4a9256a9
Jeremy Polley <jeremypolley@gmail.com>
2017-01-31 14:04:42 -0500

    remove 1 character

--- async.m
+++ async.m
@@ -2,7 +2,7 @@
 completion-scope: Function or Method
 ---
 
-dispatch_async(dispatch_get_global_queue(<#dispatch_queue_priority_t priority#>, <#unsigned long flags#>), ^(void) {
+dispatch_async(dipatch_get_global_queue(<#dispatch_queue_priority_t priority#>, <#unsigned long flags#>), ^(void) {
     <#code#>
     
     dispatch_async(dispatch_get_main_queue(), ^(void) {
