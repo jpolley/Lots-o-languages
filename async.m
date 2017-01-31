86c99a6eadcbba46fa3d9441364b9495d09a10d2
Jeremy Polley <jeremypolley@gmail.com>
2017-01-31 14:03:52 -0500

    Create async.m

---
+++ async.m
@@ -0,0 +1,8 @@
+---
+completion-scope: Function or Method
+---
+
+dispatch_async(dispatch_get_global_queue(<#dispatch_queue_priority_t priority#>, <#unsigned long flags#>), ^(void) {
+    <#code#>
+    
+    dispatch_async(dispatch_get_main_queue(), ^(void) {
